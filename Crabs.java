840c571e924824fc59ea808dfa5a69a6a9dc1d6c
slizzle <officialslarrick@gmail.com>
2016-10-05 19:14:30 -0400

    Update Crabs.java

--- Crabs.java
+++ Crabs.java
@@ -1,6 +1,7 @@
 public class Crabs{
 
   //A comment from a fork by larrickj
+  // Another comment, this time from slizzle
   
   public static void main(String[] args){
     System.out.println("Hello World");
